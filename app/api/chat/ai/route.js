9b98b2c4d1f75511bdbf8587d405d1bd907eed32
Mitesh Sodhaparmar <[sodhamitesh66@gmail.com]>
2026-02-08 12:33:24 +0530

    DeepSeek

---
+++ app/api/chat/ai/route.js
@@ -0,0 +1,62 @@
+export const maxDurationn= 60;
+import OpenAI from "openai";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { Chat } from "openai/resources/index.mjs";
+import { timeStamp } from "console";
+import connectDB from "@/config/db";
+
+// Initialize OpenAI client with DeepSeek API key and base URL
+const openai = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: process.env.DEEPSEEK_API_KEY,
+});
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req)
+
+        // Extrect chatId and prompt from the request body
+        const { chatId, prompt } = await req.json();
+
+        if (!userId) {
+            return NextResponse.json({
+                success: false,
+                message: "User not authenticated",
+            });
+        }
+
+        //Find the chat document in the database based on userId and chatId
+
+        await connectDB();
+        const data = await Chat.findOne({ userId, _id: chatId })
+
+        //Create a user message object
+        const userPrompt = {
+            role: "user",
+            content: prompt,
+            timestamp: Date.now()
+        };
+
+        data.messages.push(userPrompt);
+
+        // Call the DeepSeek api ti get a chat completion
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: prompt }],
+            model: "deepseek-chat",
+            store: true,
+        });
+
+        const message = completion.choices[0].message;
+        message.timestamp = Date.now()
+        data.messages.push(message);
+        data.save();
+
+        return NextResponse.json({success: true, data: message})
+
+    } catch (error) {
+
+        return NextResponse.json({success: false, error: error.message})
+    }
+}
---
+++ models/Chat.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+    {
+        name: {type: String, require: true},
+        messages: [
+            {
+                name: {type: String, require: true},
+                content: {type: String, require: true},
+                timestemp: {type: Number, require: true},
+            }, 
+        ],
+        userId: {ype: String, require: true},
+    },
+    {timestamps: true}
+);
+
+const Chat = mongoose.models.Chat || mongoose.model("Chat", ChatSchema)
+
+export default Chat;
