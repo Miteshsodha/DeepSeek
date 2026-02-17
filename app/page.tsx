c7b181e21a26bd26493745c317078092cb581846
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-02-17 12:06:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,6 @@ export default function Home() {
         setMessages={setMessages}
         setIsLoading={setIsLoading}
         isLoading={isLoading}
-        handleSendMessage={handleSendMessage}
       />
     </div>
   );
