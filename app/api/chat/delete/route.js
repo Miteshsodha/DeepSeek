1c8d673bcf0ca2e9e43c7ccb938464443a760b74
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-02-20 22:09:02 +0530

    DeepSeek

--- app/api/chat/delete/route.js
+++ app/api/chat/delete/route.js
@@ -1,12 +1,12 @@
 import connectDB from "@/config/db";
 import Chat from "@/models/Chat";
-import {getAuth} from "@clerk/nextjs/server";
+import {auth} from "@clerk/nextjs/server";
 import {NextResponse} from "next/server";
 import { use } from "react";
 
 export async function POST(req) {
     try {
-        const { userId } = getAuth(req);
+        const { userId } = auth();
         const {chatId} = await req.json();
 
         if(!userId) {
