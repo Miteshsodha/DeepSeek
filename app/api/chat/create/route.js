467757896a3de2fa33b4bbf96f24f57186a3c8ae
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-02-20 16:59:26 +0530

    Update route.js

--- app/api/chat/create/route.js
+++ app/api/chat/create/route.js
@@ -1,37 +1,31 @@
 import connectDB from "@/config/db";
 import Chat from "@/models/Chat";
-import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import {getAuth} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
 
-export async function POST() {
-  try {
-    const { userId } = auth();
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req)
+        if(!userId) {
+            return NextResponse.json({success: false, message: "User not authenticated",})
+        }
 
-    if (!userId) {
-      return NextResponse.json(
-        { success: false, message: "User not authenticated" },
-        { status: 401 }
-      );
+        // prepare the chat data to be saved in the database
+
+        const chatData = {
+            userId,
+            messages: [],
+            name: "New Chat",
+        };
+
+        // Connect to the database and create a new chat
+
+        await connectDB();
+        await Chat.create(chatData);
+
+        return NextResponse.json({ success: true, message: "Chat created"})
+
+    } catch (error) {
+        return NextResponse.json({ success: false, error: error.message});
     }
-
-    const chatData = {
-      userId,
-      messages: [],
-      name: "New Chat",
-    };
-
-    await connectDB();
-    await Chat.create(chatData);
-
-    return NextResponse.json(
-      { success: true, message: "Chat created" },
-      { status: 200 }
-    );
-
-  } catch (error) {
-    return NextResponse.json(
-      { success: false, error: error.message },
-      { status: 500 }
-    );
-  }
 }
